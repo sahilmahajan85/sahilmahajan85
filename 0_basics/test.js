a2f0b7439d58c438a82c07022ea016d099f5dcbb
sahilmahajan85 <mahajan.sd0108@gmail.com>
2024-10-03 17:43:35 +0000

    basics

--- 0_basics/test.js
+++ 0_basics/test.js
@@ -1,1 +1,13 @@
-console.log("Sahil")
+const accountId = 1513251 // cannot be changed once assigned  
+let accountEmail ="mahajan.sd@gmail.com" // can be changed
+var accountPassword = "12345" // cannot used nowadays 
+accountCity = "jaipur" // it makes a default var
+let accountState; 
+
+accountEmail ="sahilmahajan@gmail.com" 
+ accountPassword = "1111"
+accountCity = "Goa"
+
+console.log(accountEmail);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
