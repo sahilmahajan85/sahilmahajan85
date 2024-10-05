e04e2c352c03feb3cf3c8d477dfdac66c37c7991
sahilmahajan85 <mahajan.sd0108@gmail.com>
2024-10-05 17:02:04 +0000

    basics

--- 0_basics/test.js
+++ 0_basics/test.js
@@ -96,3 +96,18 @@ const num =23.8966
 console.log(num.toPrecision(3));
   
 console.log((Math.random()*10)+1);
+
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date("01-14-2023")
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
